e70ad7683a5ff68d96a6b8d621968d5a9dc077ce
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-25 16:31:32 +0100

    init react app

---
+++ src/components/Venue.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './component_css/venue.css';
+
+class Venue extends Component {
+  render() {
+    return (
+      <div className="venue">
+        <div className="venue-picture">
+          <div />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Venue;
